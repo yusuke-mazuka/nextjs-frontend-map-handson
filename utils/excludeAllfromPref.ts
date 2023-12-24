52defaad87f303603de32a4f1e89399a1e57ff65
yusuke-mazuka <ymazuka@redhat.com>
2023-12-24 17:23:47 +0900

    「全国」を表す都道府県コードを定数化

--- utils/excludeAllfromPref.ts
+++ utils/excludeAllfromPref.ts
@@ -1,8 +1,11 @@
-import { PrefPopulation } from "@/types/Response";
+import { PrefPopulation } from '@/types/Response';
+
+// 「全国」を表す都道府県コード
+const WHOLE_COUNTRY_PREF_CODE = '00';
 
 /**
  * 県別のデータから「全国」のデータを除外します
  */
 export const excludeAllfromPref = (prefList: PrefPopulation[]) => {
-  return prefList.filter((pref) => pref.prefCode !== "00");
+  return prefList.filter((pref) => pref.prefCode !== WHOLE_COUNTRY_PREF_CODE);
 };
